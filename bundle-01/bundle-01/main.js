d34666e8ae449becf5ba36d23bcdb5dd53e32921
alessandrodamato <alessandro042004@gmail.com>
2024-03-08 15:26:27 +0100

    [done] bundle 1

--- bundle-01/bundle-01/main.js
+++ bundle-01/bundle-01/main.js
@@ -11,37 +11,49 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
-    console.log(i);
+for (let i = 0; i < 5; i++) {
+    console.log('es 1 ----',i);
 }
-
+// errore: "i" non potrà mai essere maggiore di 5, per far funzionare il ciclo deve essere < 5
 
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
-        return num + 5;
-    }
-    return num;
+  if (num % 2 === 0) {
+    return num + 5;
+  }
+  return num;
 }
-
+console.log('Es 2-------------',addIfEven(2), addIfEven(3));
+//errore: num % 2 = 0: un uguale solo è di assegnazione non di comparazione, adesso se è pari lo stampa sommato a 5 altrimenti rimane il numero inserito
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
-        console.log(i);
-    }
+  for (let i = 0; i < 5; i++) {
+    console.log('Es 3------------------',i);
+  }
 }
-
+loopToFive()
+//l'errore erano le virgole "," invece dei punti e virgola ";" - errore di sintassi
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-    let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
-        }
-        return evenNumbers;
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  let evenNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    let n = numbers[i];
+    if (n % 2 === 0){
+        evenNumbers.push(n);
+      }
     }
+    return console.log(evenNumbers);
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+//errori :
+// 1. i++;
+//2. numbers % 2 = 0
+//3.  numbers.length - 1
+//4. ; dopo la condizione dell'if
+//5. il return va fuori dal ciclo for
+//6. va pushato il numero singolo con indice pari
+//7. il return deve essere anche il log dell'array
